c616bd56e256a356c4ae48e6d1d72012737ed09e
Leandro Herbas <ophrbs@gmail.com>
2023-05-12 07:00:25 -0300

    remove scanner

--- 3POB/Exc1 - Calculadora/src/Calculadora/Main.java
+++ 3POB/Exc1 - Calculadora/src/Calculadora/Main.java
@@ -1,9 +1,7 @@
 package Calculadora;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Calculadora calculadora = new Calculadora();
 
         calculadora.iniciar();
